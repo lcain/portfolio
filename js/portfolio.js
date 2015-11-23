b19e9ed53b58dcf07d7bb410d6f08d0c599f4fb0
Loretta Cain <locacodes@gmail.com>
2015-11-23 12:12:46 +1100

    Different sized bubbles.

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,14 +3,19 @@ $(document).ready(function () {
     // console.log(event.clientX, event.clientY)
 
     var $b = $('<div/>').addClass('bubble');
+
+    var size = Math.random() * 4;
+
     $b.css({
+      width: size + 'em',
+      height: size + 'em',
       left: event.clientX,
       top: event.clientY 
     });
     $('body').append($b);
     //Clean up after ourselves.
     setTimeout(function() {
-      
+
       $b.animate({
        top: -1000  //Dissapear off the top of the screen
       }, 2000, function() {
